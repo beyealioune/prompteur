44105db1954caa5e7b6a2e361531420342733c10
Alioune Beye <alioune@Host-004.lan>
2025-04-15 13:14:48 +0200

    ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,18 @@
-const config = {
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
   appId: 'com.aliapp.prompteur',
   appName: 'PrompteurApp',
   webDir: 'dist/prompteur/browser',
-  bundledWebRuntime: false
+  plugins: {
+    Camera: {
+      cameraPermission: 'Nécessite l\'accès à la caméra',
+      microphonePermission: 'Nécessite l\'accès au micro'
+    },
+    VideoRecorder: {
+      microphonePermission: 'Nécessite l\'accès au micro'
+    }
+  }
 };
 
 export default config;
