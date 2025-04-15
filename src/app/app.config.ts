dd7dcbd1a6a66f10f6e7448b32714bd53a165942
Alioune Beye <alioune@Host-004.lan>
2025-04-15 12:55:26 +0200

    ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptor/jwt.interceptor';
 
+import { IonicModule } from '@ionic/angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]),), provideAnimationsAsync(), 
-  ]};
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimationsAsync(),
+    importProvidersFrom(IonicModule.forRoot()) // ✅ Garde uniquement ça pour Ionic
+  ]
+};
