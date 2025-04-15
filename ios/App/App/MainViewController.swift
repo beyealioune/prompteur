25c5ec9e619a63bf94b6cf969f83e8da91bf4854
Alioune Beye <alioune@Host-004.lan>
2025-04-15 10:21:57 +0200

    mainview

--- ios/App/App/MainViewController.swift
+++ ios/App/App/MainViewController.swift
@@ -8,6 +8,7 @@ class MainViewController: CAPViewController {
         let config = WKWebViewConfiguration()
         config.allowsInlineMediaPlayback = true
         config.mediaTypesRequiringUserActionForPlayback = []
+    print("✅ MainViewController chargé avec lecture inline activée")
 
         self.webView = WKWebView(frame: .zero, configuration: config)
         super.loadView()
