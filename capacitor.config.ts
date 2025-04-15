88edd8dfa3bb79214e46ce561bca342c7925b776
Alioune Beye <alioune@Host-004.lan>
2025-04-15 11:17:21 +0200

    ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,8 @@
 const config = {
   appId: 'com.aliapp.prompteur',
   appName: 'PrompteurApp',
-  webDir: 'dist/prompteur/browser', // ✅ c’est bien ici ton index.html
-  plugins: {
-    Camera: {
-      cameraPermission: 'Cette application nécessite l\'accès à la caméra',
-      microphonePermission: 'Cette application nécessite l\'accès au micro'
-    }
-  }
+  webDir: 'dist/prompteur/browser',
+  bundledWebRuntime: false
 };
 
 export default config;
