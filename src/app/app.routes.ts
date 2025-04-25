c71d4ff13e3ecbef164a57096cdea3e89f4cf35c
Alioune Beye <alioune@Host-003.lan>
2025-04-25 15:30:53 +0200

    test mise en place bouton et plein écran

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,6 @@ import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-co
 import { FaqComponent } from './faq/faq.component';
 import { PolitiqueConfidentialiteComponent } from './politique-confidentialite/politique-confidentialite.component';
 import { StripeSuccessComponent } from './stripe-success/stripe-success.component';
-import { CameratestComponent } from './cameratest/cameratest.component';
 
 
 export const routes: Routes = [
@@ -26,7 +25,6 @@ export const routes: Routes = [
   { path: 'profil', component: ProfilComponent, canActivate: [AuthGuard] },
   { path: 'prompteur', component: PrompteurComponent, canActivate: [AuthGuard] },
   { path: 'stripe-success', component: StripeSuccessComponent },
-  {path: 'test', component: CameratestComponent},
 
   {
     path: 'connexion',
