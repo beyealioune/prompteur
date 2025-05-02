3ebf6db7f35c657fbba4c6d511483a024eadfbb3
9109898N <alioune.beye@sncf.fr>
2025-05-02 17:06:28 +0200

    maj apres review apple

--- src/app/services/profil.service.ts
+++ src/app/services/profil.service.ts
@@ -21,9 +21,10 @@ export class ProfilService {
   updatePassword(password: string): Observable<any> {
     return this.http.put(`${this.baseUrl}users/auth/password`, { password });
   }
-  deleteAccount(userId: number) {
-    return this.http.delete(`${environment.apiUrl}user/${userId}`);
+  deleteAccount(userId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}auth/delete-account/${userId}`);
   }
+  
   
   uploadPhoto(userId: number, file: File): Observable<any> {
     const formData = new FormData();
