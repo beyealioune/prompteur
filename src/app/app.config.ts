b4c426b40007146e65e95f8935201184d4d9e1fb
Alioune Beye <alioune@Host-006.lan>
2025-05-23 22:59:13 +0200

    correction

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { routes } from './app.routes';
 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptor/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { IonicModule } from '@ionic/angular';
 import { provideRouter } from '@angular/router';
@@ -15,7 +16,4 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(IonicModule.forRoot()) // ✅ Garde uniquement ça pour Ionic
   ]
 };
-function provideAnimationsAsync(): import("@angular/core").Provider | import("@angular/core").EnvironmentProviders {
-  throw new Error('Function not implemented.');
-}
 
