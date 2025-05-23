b26b21dd6021eef352fd6880b16ab9f8108c8091
Alioune Beye <alioune@Host-001.lan>
2025-05-23 12:04:09 +0200

    ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,37 +1,23 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aliapp.prompteur',
   appName: 'PrompteurApp',
   webDir: 'dist/prompteur/browser',
+  ios: {
+    scheme: 'App'
+  },
   plugins: {
-    Camera: {
-      cameraPermission: 'Nécessite l\'accès à la caméra',
-      microphonePermission: 'Nécessite l\'accès au micro'
-    },
-    VideoRecorder: {
-      microphonePermission: 'Nécessite l\'accès au micro'
-    },
-    // Ajoutez la configuration pour les achats in-app
     Purchase: {
       ios: {
         products: [
           {
             id: 'prompteur_1_9',
-            type: 'PAID_SUBSCRIPTION' // ou 'CONSUMABLE'/'NON_CONSUMABLE' selon le type
+            type: 'PAID_SUBSCRIPTION'
           }
         ]
-      },
-      android: {
-        // Configuration Android si nécessaire
       }
     }
-  },
-  // Optionnel: Configuration iOS supplémentaire
-  ios: {
-    scheme: 'App',
-    preferredContentMode: 'mobile',
-    cordovaLinkerFlags: ['-ObjC']
   }
 };
 
