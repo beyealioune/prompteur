464269bc20856c0b5ed5657ca7257a10962bae9a
Alioune Beye <alioune@Host-006.lan>
2025-05-28 14:39:34 +0200

    wip

--- src/app/payment-popup/payment-popup.component.ts
+++ src/app/payment-popup/payment-popup.component.ts
@@ -3,7 +3,6 @@ import { AlertController, IonicModule, Platform } from '@ionic/angular';
 import { PaymentService } from '../services/payment.service';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { InAppPurchase2, IAPProduct } from '@ionic-native/in-app-purchase-2/ngx';
 
 
 declare global {
@@ -18,130 +17,60 @@ const PRODUCT_KEY = 'prompteur_1_9';
 @Component({
   selector: 'app-payment-popup',
   standalone: true,
-  imports: [CommonModule, RouterLink, IonicModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './payment-popup.component.html',
   styleUrl: './payment-popup.component.css'
 })
 export class PaymentPopupComponent {
-  // @Output() close = new EventEmitter<void>();
-  // private paymentService = inject(PaymentService);
-  // private platform = inject(Platform);
+   @Output() close = new EventEmitter<void>();
+  private paymentService = inject(PaymentService);
+   private platform = inject(Platform);
 
-  // public get isStoreReady(): boolean {
-  //   return this.paymentService.isStoreReady;
-  // }
+   public get isStoreReady(): boolean {
+     return this.paymentService.isStoreReady;
+   }
 
-  // public get productLoaded(): boolean {
-  //   return this.paymentService.productLoaded;
-  // }
+   public get productLoaded(): boolean {
+     return this.paymentService.productLoaded;
+   }
 
-  // public isIOS(): boolean {
-  //   return this.platform.is('ios') || /iPad|iPhone|iPod/.test(navigator.userAgent);
-  // }
+   public isIOS(): boolean {
+     return this.platform.is('ios') || /iPad|iPhone|iPod/.test(navigator.userAgent);
+   }
 
-  // onTryFree(): void {
-  //   if (this.isIOS()) {
-  //     this.paymentService.activateIosTrial().subscribe({
-  //       next: () => alert("✅ Essai gratuit activé (iOS) !"),
-  //       error: (err) => alert("Erreur activation iOS trial : " + err.message)
-  //     });
-  //   } else {
-  //     this.paymentService.createTrialSession().subscribe({
-  //       next: (res) => window.location.href = res.url,
-  //       error: (err) => alert('Erreur Stripe : ' + err.message)
-  //     });
-  //   }
-  // }
-
-  // onPayNow(): void {
-  //   if (this.isIOS()) {
-  //     this.paymentService.startApplePurchase('prompteur_1_9');
-  //   } else {
-  //     this.paymentService.createImmediateSession().subscribe({
-  //       next: (res) => window.location.href = res.url,
-  //       error: (err) => alert('Erreur Stripe : ' + err.message)
-  //     });
-  //   }
-  // }
-
-  // onClose(): void {
-  //   this.close.emit();
-  // }
-
-  // refreshStore(): void {
-  //   this.paymentService.refreshStore();
-  // }
-
-
-
-
-  products: IAPProduct[] = [];
-  isSubscribed = false;
-  public product: any;
-
-  constructor(
-    private plt: Platform,
-    private store: InAppPurchase2,
-    private alertController: AlertController,
-    private ref: ChangeDetectorRef,
-    private iap: InAppPurchase2 // <= ici
-
-  ) {
-    this.plt.ready().then(() => {
-      this.store.verbosity = this.store.DEBUG;
-
-      this.registerProducts();
-      this.setupListeners();
-
-      this.store.ready(() => {
-        this.products = this.store.products;
-        this.ref.detectChanges();
-      });
-    });
+   onTryFree(): void {
+    if (this.isIOS()) {
+      this.paymentService.activateIosTrial().subscribe({
+         next: () => alert("✅ Essai gratuit activé (iOS) !"),
+         error: (err) => alert("Erreur activation iOS trial : " + err.message)
+       });
+     } else {
+       this.paymentService.createTrialSession().subscribe({
+        next: (res) => window.location.href = res.url,
+         error: (err) => alert('Erreur Stripe : ' + err.message)
+     });
+    }
   }
 
-  registerProducts() {
-    this.store.register({
-      id: PRODUCT_KEY,
-      type: this.store.PAID_SUBSCRIPTION,
-    });
+ onPayNow(): void {
+   if (this.isIOS()) {
+      this.paymentService.startApplePurchase('prompteur_1_9');
+    } else {
+      this.paymentService.createImmediateSession().subscribe({
+        next: (res) => window.location.href = res.url,
+        error: (err) => alert('Erreur Stripe : ' + err.message)
+       });
+     }
+   }
 
-    this.store.refresh();
-  }
+   onClose(): void {
+     this.close.emit();
+   }
 
-  setupListeners() {
-    this.store.when(PRODUCT_KEY)
-      .approved((p: IAPProduct) => {
-        this.isSubscribed = true;
-        this.ref.detectChanges();
-        return p.verify();
-      })
-      .verified((p: IAPProduct) => p.finish());
+   refreshStore(): void {
+    this.paymentService.refreshStore();
+   }
 
-    this.store.when(PRODUCT_KEY)
-      .owned((p: IAPProduct) => {
-        this.isSubscribed = true;
-      });
-  }
 
-  purchase(product: IAPProduct) {
-    this.store.order(product).then((p: any) => {
-      // Traitement après achat
-    }, (e: any) => {
-      this.presentAlert('Failed', `Failed to purchase: ${e}`);
-    });
-  }
 
-  restore() {
-    this.store.refresh();
-  }
-
-  async presentAlert(header: string, message: string) {
-    const alert = await this.alertController.create({
-      header,
-      message,
-      buttons: ['OK']
-    });
-    await alert.present();
-  }
 }
