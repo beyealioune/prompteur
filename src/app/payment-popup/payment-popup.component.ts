52d8d3c6f06e02a6c12c9c06a005b54f11150dfa
9109898N <alioune.beye@sncf.fr>
2025-05-30 10:12:22 +0200

    nouveau test

--- src/app/payment-popup/payment-popup.component.ts
+++ src/app/payment-popup/payment-popup.component.ts
@@ -22,50 +22,6 @@ const PRODUCT_KEY = 'prompteur_1_9';
   styleUrl: './payment-popup.component.css'
 })
 export class PaymentPopupComponent {
-   @Output() close = new EventEmitter<void>();
-  private paymentService = inject(PaymentService);
-   private platform = inject(Platform);
-
-   public get isStoreReady(): boolean {
-     return this.paymentService.isStoreReady;
-   }
-
-   public get productLoaded(): boolean {
-     return this.paymentService.productLoaded;
-   }
-
-   public isIOS(): boolean {
-     return this.platform.is('ios') || /iPad|iPhone|iPod/.test(navigator.userAgent);
-   }
-
-   onTryFree(): void {
-    if (this.isIOS()) {
-      this.paymentService.activateIosTrial().subscribe({
-         next: () => alert("✅ Essai gratuit activé (iOS) !"),
-         error: (err) => alert("Erreur activation iOS trial : " + err.message)
-       });
-     } else {
-       this.paymentService.createTrialSession().subscribe({
-        next: (res) => window.location.href = res.url,
-         error: (err) => alert('Erreur Stripe : ' + err.message)
-     });
-    }
-  }
-
- onPayNow(): void {
-   if (this.isIOS()) {
-      this.paymentService.startApplePurchase('prompteur_1_9');
-    } else {
-      this.paymentService.createImmediateSession().subscribe({
-        next: (res) => window.location.href = res.url,
-        error: (err) => alert('Erreur Stripe : ' + err.message)
-       });
-     }
-   }
-
-   onClose(): void {
-     this.close.emit();
-   }
 
   // refreshStore(): void {
   //   this.paymentService.refreshStore();
